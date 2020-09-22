32a700ee800482fe943f7c42e8fcb3e228534c84
Arturo Rivera <a.rivera13@hotmail.com>
2020-09-21 22:28:52 -0700

    Heroku Checkpoint Complete

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 const app = require("./app");
 const http = require("http");
-const server = http.createServer(app);
 
-server.listen(3000);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+function normalizePort(value) {
+
+  const port = parseInt(value, 10);
+  if(isNaN(port)) {
+    return value;
+  }
+
+  if(port >= 0) {
+    return port;
+  }
+
+  return false;
+}
 
 server.on("listening", () => {
   console.log("Server is listening for requests on port 3000");
