4324bfea4e431fcd7d42d34fbdc365f0c2c6e5ae
Arturo Rivera <a.rivera13@hotmail.com>
2020-09-24 20:44:17 -0700

    Sequelize Checkpoint Complete

---
+++ src/db/models/topic.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Topic extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Topic.hasMany(models.Banner, {
+        foreignKey: "topicId",
+        as: "banners"
+      });
+    }
+  };
+  Topic.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Topic',
+  });
+  return Topic;
+};
